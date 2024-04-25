878b33f1599259b63bb9232e770ba2609cbfeccd
Adolberton <7557@holbertonstudents.com>
2024-04-25 16:18:37 +0200

    auto commit

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Tasks"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Tasks"""
+    return asyncio.create_task(wait_random(max_delay))
