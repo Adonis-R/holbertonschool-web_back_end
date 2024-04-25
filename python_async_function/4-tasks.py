839b35218ab4a718d7d4404e714e51f6370b8c51
Adolberton <7557@holbertonstudents.com>
2024-04-25 16:23:09 +0200

    auto commit

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+""" Concurrent coroutines """
+
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ Returns list of delays in ascending order """
+    tasks = []
+    delays = []
+    for _ in range(n):
+        task = task_wait_random(max_delay) 
+        tasks.append(task)
+
+    for task in tasks:
+        delays.append(await task)
+
+    return sorted(delays)
