e237931434a40a159247a9fe20ab46d4c4e88639
Adolberton <7557@holbertonstudents.com>
2024-04-25 16:24:20 +0200

    auto commit

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,12 +6,13 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ Returns list of delays in ascending order """
     tasks = []
     delays = []
     for _ in range(n):
-        task = task_wait_random(max_delay) 
+        task = task_wait_random(max_delay)
         tasks.append(task)
 
     for task in tasks:
