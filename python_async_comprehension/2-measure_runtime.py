8cef792bd46d426b5f75a09238063d88cf69d9e4
Adolberton <7557@holbertonstudents.com>
2024-04-26 12:05:58 +0200

    auto commit

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """2. Run time for four parallel comprehensions"""
 import asyncio
-import random
 import time
 from typing import List
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Measure the runtime of async_comprehension executed 4 times in parallel"""
+    """Measure the runtime of async_comprehension
+    executed 4 times in parallel"""
+
     start = time.time()
     await asyncio.gather(
         async_comprehension(),
