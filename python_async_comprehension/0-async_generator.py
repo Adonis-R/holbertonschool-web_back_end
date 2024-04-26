f0b76c174c2e9cd5d3c015b51092bc3cdc7f57c6
Adolberton <7557@holbertonstudents.com>
2024-04-26 11:08:29 +0200

    auto commit

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def async_generator() -> float:
+async def async_generator():
     """Yield a random number between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
