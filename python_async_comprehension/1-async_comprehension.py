5bb7f1eb5f3458d6583dbd9a3b5c6ee9780de305
Adolberton <7557@holbertonstudents.com>
2024-04-26 12:01:04 +0200

    auto commit

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""1. Async Comprehensions"""
+import asyncio
+import random
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Collect 10 random numbers using an async comprehensing over async_generator"""
+    return [i async for i in async_generator()]
--- python_async_function/test.py
+++ python_async_function/test.py
@@ -1,7 +1,10 @@
+
+#!/usr/bin/env python3
+
 import asyncio
 
-wait_n = __import__('1-concurrent_coroutines').wait_n
+task_wait_n = __import__('4-tasks').task_wait_n
 
-print(asyncio.run(wait_n(5, 5)))
-print(asyncio.run(wait_n(10, 7)))
-print(asyncio.run(wait_n(10, 0)))
+n = 5
+max_delay = 6
+print(asyncio.run(task_wait_n(n, max_delay)))
