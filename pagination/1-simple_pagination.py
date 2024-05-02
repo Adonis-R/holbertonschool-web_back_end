0b0e679b7c8afbffcd02dbabecf0173c3c807bad
Adolberton <7557@holbertonstudents.com>
2024-05-02 11:56:30 +0200

    auto commit

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Simple pagination"""
+
+import csv
+import math
+from typing import List, Tuple
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get a page with pagination"""
+        assert type(page) == int and type(page_size) == int, "arg(s) of get_page != int"
+        assert page > 0 and page_size > 0, "page and page_size must be > 0"
+        start, end = index_range(page, page_size)
+        idx = []
+        self.dataset()
+        for i in range(start, end):
+            try: 
+                idx.append(self.__dataset[i])
+            except IndexError as e:
+                return []
+        return idx
