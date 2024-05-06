e6b622de0e532eaf46896caee88782a147b98fcd
Adolberton <7557@holbertonstudents.com>
2024-05-06 15:21:00 +0200

    auto commit

--- ES6_basic/0-main.js
+++ ES6_basic/0-main.js
@@ -1,3 +1,3 @@
-import { taskFirst, taskNext } from './0-constants.js';
+import concatArrays from './5-spread-operator.js';
 
-console.log(`${taskFirst()} ${taskNext()}`);
+console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
