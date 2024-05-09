c68b7b18629795cee79913525af7d83f2bcae23e
Adolberton <7557@holbertonstudents.com>
2024-05-09 19:14:39 +0200

    auto commit

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -32,6 +32,7 @@ export class StudentHolberton {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
+
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -42,4 +43,5 @@ const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 const listOfStudents = [student1, student2, student3, student4, student5];
+
 export default listOfStudents;
