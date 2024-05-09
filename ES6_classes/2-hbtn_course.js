5fb31910a55bbb5f242faea7bfde4097f5ff9285
Adolberton <7557@holbertonstudents.com>
2024-05-09 15:10:00 +0200

    auto commit

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,7 +1,52 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-        this._name = name;
-        this._lenght = length;
-        this._students = students;
+  constructor(name, length, students) {
+    this._name = HolbertonCourse.validateString(name, 'Name');
+    this._length = HolbertonCourse.validateNumber(length, 'Length');
+    this._students = HolbertonCourse.validateArray(students, 'Students');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = HolbertonCourse.validateString(newName, 'Name');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    this._length = HolbertonCourse.validateNumber(newLength, 'Length');
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    this._students = HolbertonCourse.validateArray(newStudents, 'Students');
+  }
+
+  static validateString(value, propertyName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${propertyName} Must be a string`);
     }
+    return value;
+  }
+
+  static validateNumber(value, propertyName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${propertyName} Must be a number`);
+    }
+    return value;
+  }
+
+  static validateArray(value, propertyName) {
+    if (!Array.isArray(value)) {
+      throw new TypeError(`${propertyName} Must be an array`);
+    }
+    return value;
+  }
 }
--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,1 +1,31 @@
 /* eslint-disable */
+import Currency from "./3-currency.js";
+
+export default class Pricing {
+    constructor(amount, currency) {
+        this._amount = amount;
+        this._currency = currency;
+    }
+
+    set amount(newAmount) {
+        this._amount = newAmount;
+    }
+
+    set currency(newCurrency) {
+        this._curency = newCurrency;
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+    displayFullPrice() {
+        return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+    }
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
+}
