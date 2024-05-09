f677f0cf6a60565f0d1d29ed9f6694d2a2d753ac
Adolberton <7557@holbertonstudents.com>
2024-05-09 18:59:42 +0200

    auto commit

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -2,30 +2,30 @@
 import Currency from "./3-currency.js";
 
 export default class Pricing {
-    constructor(amount, currency) {
-        this._amount = amount;
-        this._currency = currency;
-    }
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-    set amount(newAmount) {
-        this._amount = newAmount;
-    }
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
 
-    set currency(newCurrency) {
-        this._curency = newCurrency;
-    }
+  set currency(newCurrency) {
+    this._curency = newCurrency;
+  }
 
-    get amount() {
-        return this._amount;
-    }
+  get amount() {
+    return this._amount;
+  }
 
-    get currency() {
-        return this._currency;
-    }
-    displayFullPrice() {
-        return `${this.amount} ${this.currency.name} (${this.currency.code})`;
-    }
-    static convertPrice(amount, conversionRate) {
-        return amount * conversionRate;
-    }
+  get currency() {
+    return this._currency;
+  }
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,1 +1,21 @@
 /* eslint-disable */
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(newFloors) {
+    this._floors = newFloors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
