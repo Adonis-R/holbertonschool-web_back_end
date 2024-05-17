1d1357acfaed048291ddd03c62d3494937bf6570
Adolberton <7557@holbertonstudents.com>
2024-05-17 15:18:05 +0200

    auto commit

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
+  return promise.then(() => {
     console.log('Got a response from the API');
-    return {
-      status: 200,
-      body: 'success',
-    };
-  }).catch(() => new Error());
+    return { status: 200, body: 'success' };
+  })
+    .catch(() => new Error());
 }
