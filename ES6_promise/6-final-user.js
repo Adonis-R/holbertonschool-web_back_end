6f5b32631cf1d9743f90ab74c1424b066eea1dc7
Adolberton <7557@holbertonstudents.com>
2024-05-17 15:11:49 +0200

    auto commit

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      const promises = [];
+      results.forEach((promise) => {
+        promises.push({
+          status: promise.status,
+          value: promise.status === 'fulfilled' ? promise.value : `${promise.reason.name}: ${promise.reason.message}`,
+        });
+      });
+      return promises;
+    });
+}
