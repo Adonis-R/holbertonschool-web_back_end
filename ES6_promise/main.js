b8d9929d6eb67bdd99fedcb543ac3b8b430db078
Adolberton <7557@holbertonstudents.com>
2024-05-17 14:52:21 +0200

    auto commit

--- ES6_promise/main.js
+++ ES6_promise/main.js
@@ -1,12 +1,11 @@
 const name = 'Alice';
-let age = 30;
+const age = 30;
 const hobbies = ['reading', 'traveling', 'swimming'];
 
 function introduce(personName, personAge, personHobbies) {
-  console.log('Name: ' + personName);
-  console.log('Age: ' + personAge);
-  console.log('Hobbies: ' + personHobbies.join(', '));
+  console.log(`Name: ${personName}`);
+  console.log(`Age: ${personAge}`);
+  console.log(`Hobbies: ${personHobbies.join(', ')}`);
 }
 
 introduce(name, age, hobbies);
-
