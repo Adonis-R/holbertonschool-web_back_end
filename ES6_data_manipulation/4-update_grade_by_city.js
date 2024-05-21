0da1369cf445fd4b014b113e7afbc8944c472662
Adolberton <7557@holbertonstudents.com>
2024-05-21 16:56:56 +0200

    auto commit

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,9 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const foundGrade = newGrades.find((grade) => grade.studentId === student.id);
-      if (foundGrade) {
-        return { ...student, grade: foundGrade.grade };
-      }
-      return { ...student, grade: 'N/A' };
-    });
+  return students.filter((student) => student.location === city).map((student) => {
+    const updatedGrade = newGrades.find((grade) => grade.studentId === student.id);
+    if (updatedGrade) {
+      return { ...student, grade: updatedGrade.grade };
+    }
+    return { ...student, grade: 'N/A' };
+  });
 }
