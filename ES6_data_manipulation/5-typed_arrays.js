738fa80b0acfa9a61e998002e43a2f125a77d39b
Adolberton <7557@holbertonstudents.com>
2024-05-21 14:20:39 +0200

    auto commit

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
-  if (position < 0 || position >= int8Array.length) {
+  const dataView = new DataView(buffer);
+  if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
-  int8Array[position] = value;
+  dataView.setInt8(position, value);
   return buffer;
 }
