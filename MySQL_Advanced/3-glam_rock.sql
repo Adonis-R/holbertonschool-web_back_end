d45ef4a6c0219665566ea38b3dcf700b2ac6c317
Adonis-R <adonisriahi@gmail.com>
2025-07-29 10:28:42 +0200

    Fix lifespan calculation in Glam rock SQL script by updating default split year to 2022

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity
 -- Uses formed and split attributes to compute lifespan in years
-SELECT band_name, (IFNULL(split, 2025) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
