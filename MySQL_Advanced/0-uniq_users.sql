203769de4b77d944615e10b6bee30f06fbdf405f
Adonis-R <adonisriahi@gmail.com>
2025-07-29 09:20:04 +0200

    Add comments to clarify users table creation in SQL script

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,5 @@
+-- Creates a table users with id, email, and name attributes
+-- id is auto increment primary key, email is unique and not null
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
